5f7d6bc63af0f31096c7a4ad2ec037a006009525
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-01-23 11:13:07 +0530

    cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const connectToMongo = require("./db")
 const express = require("express")
+const cors = require("cors")
+
 
 connectToMongo()
 
 const app = express()
 const port = 5132
 
+app.use(cors())
 app.use(express.json())
 
 // Routes
